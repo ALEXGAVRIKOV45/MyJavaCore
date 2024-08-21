327a6d7c869b56874f0770c3e8bd80aacf517e19
ALEXGAVRIKOV45 <alex_gavrikov@rambler.ru>
2024-08-21 18:00:10 +0500

    Commite 4 некоторые покинули зоопарк

--- src/main/java/supportClasses/AppZoo.java
+++ src/main/java/supportClasses/AppZoo.java
@@ -4,6 +4,7 @@ import abstractClass.Animal;
 import zooClasses.*;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import static supportClasses.AnimalStatus.HEBRIVORE;
 import static supportClasses.AnimalStatus.PREDATOR;
@@ -12,23 +13,35 @@ import static supportClasses.AnimalType.*;
 public class AppZoo {
     public static ArrayList<Animal> initAnimals() {
         ArrayList<Animal> arrAnimals = new ArrayList<Animal>();
-        arrAnimals.add(new Wolf(WOLF,"Серый", 7, PREDATOR));
-        arrAnimals.add(new Deer(DEER,"Олешка", 3, HEBRIVORE));
-        arrAnimals.add(new Deer(DEER,"Рогатый", 4, HEBRIVORE));
-        arrAnimals.add(new Giraffe(GIRAFFE,"Длинный", 5, HEBRIVORE));
-        arrAnimals.add(new Wolf(WOLF,"Злой", 10, PREDATOR));
-        arrAnimals.add(new Tiger(TIGER,"Царь саванны", 8, PREDATOR));
-        arrAnimals.add(new Bear(BEAR,"Косолапый", 10, PREDATOR));
-        arrAnimals.add(new Ostrich(OSTRICH,"Быстрый", 6, HEBRIVORE));
+        arrAnimals.add(new Wolf(WOLF, "Серый", 7, PREDATOR));
+        arrAnimals.add(new Deer(DEER, "Олешка", 3, HEBRIVORE));
+        arrAnimals.add(new Deer(DEER, "Рогатый", 4, HEBRIVORE));
+        arrAnimals.add(new Giraffe(GIRAFFE, "Длинный", 5, HEBRIVORE));
+        arrAnimals.add(new Wolf(WOLF, "Злой", 10, PREDATOR));
+        arrAnimals.add(new Tiger(TIGER, "Царь саванны", 8, PREDATOR));
+        arrAnimals.add(new Bear(BEAR, "Косолапый", 10, PREDATOR));
+        arrAnimals.add(new Ostrich(OSTRICH, "Быстрый", 6, HEBRIVORE));
 
         return arrAnimals;
     }
 
-    public static void  printZoo(ArrayList<Animal> arrAnimals) {
+    public static void printZoo(ArrayList<Animal> arrAnimals) {
         arrAnimals.forEach(System.out::println);
     }
 
-    public static void  voiceZoo(ArrayList<Animal> arrAnimals) {
-        arrAnimals.forEach(animal -> {animal.getVoice();});
+    public static void voiceZoo(ArrayList<Animal> arrAnimals) {
+        arrAnimals.forEach(Animal::getVoice);
     }
+
+    public static ArrayList<Animal> predatorsZoo(ArrayList<Animal> arrAnimals) {
+         return (ArrayList<Animal>) arrAnimals.stream()
+                .filter(animal -> (animal.getSpecies() == PREDATOR))
+                .collect(Collectors.toList());
+    }
+
+    public static void sellAnimal(String nameRemove, ArrayList<Animal> arrAnimals) {
+        arrAnimals.removeIf(animal -> animal.getName().equalsIgnoreCase(nameRemove));
+    }
+
+
 }
--- src/main/java/zooClasses/Bear.java
+++ src/main/java/zooClasses/Bear.java
@@ -8,4 +8,8 @@ public class Bear extends Animal {
     public Bear(AnimalType type, String name, int age, AnimalStatus species) {
         super(type, name, age, species);
     }
+    @Override
+    public void getVoice() {
+        System.out.println("Рев медведя");
+    }
 }
